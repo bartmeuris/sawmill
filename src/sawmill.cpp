c69e28a1d854148f40f492615774318c0ce5de1c
bartmeuris <bart.meuris@gmail.com>
2013-01-21 19:39:38 +0100

    Added basic commandline parameter parsing

--- src/sawmill.cpp
+++ src/sawmill.cpp
@@ -1,9 +1,34 @@
+#include <string>
 #include <iostream>
+#include <boost/program_options.hpp>
+
 #include "command.pb.h"
 #include "logevent.pb.h"
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[])
 {
+	// Parse commandline arguments
+	po::options_description desc("Generic options");
+	desc.add_options()
+		("help,h", "Show this help message")
+		("config,c", po::value<std::string>(), "Specify a config file to use")
+	;
+	po::variables_map vm;
+	try {
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+	} catch (po::error &e) {
+		std::cerr << "Commandline error: " << e.what() << std::endl;
+		std::cout << desc << std::endl;
+		return 1;
+	}
+	if ( (argc <= 1) || (vm.count("help")) ) {
+		std::cout << desc << std::endl;
+		return 1;
+	}
+
 	std::cout << "Test" << std::endl;
 	return 0;
 }
