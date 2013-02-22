f5903f40e399d3791fc66331ff0e5ed935d37d62
bartmeuris <bart.meuris@gmail.com>
2013-02-22 13:45:39 +0100

    Initial commit of config.h

---
+++ src/config.h
@@ -0,0 +1,22 @@
+#ifndef __CONFIG_H_LOADED
+# define __CONFIG_H_LOADED
+
+#ifndef APP_NAME
+# define APP_NAME "sawmill"
+#endif
+
+#ifndef VER_MAJOR
+# define VER_MAJOR 0
+#endif
+#ifndef VER_MINOR
+# define VER_MINOR 0
+#endif
+#ifndef OS_NAME
+# define OS_NAME "unknown"
+#endif
+
+#ifndef SM_DEFAULT_CONFIGFILE
+# define SM_DEFAULT_CONFIGFILE "/etc/sawmill/sawmill.conf"
+#endif
+
+#endif
