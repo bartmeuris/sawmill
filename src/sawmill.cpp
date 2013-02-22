6e0b5f513069311194e820bcaa68bdc38c78f5c2
bartmeuris <bart.meuris@gmail.com>
2013-02-22 13:45:57 +0100

    Added SawMill class, added header, ...

--- src/sawmill.cpp
+++ src/sawmill.cpp
@@ -1,21 +1,69 @@
+/****************************************************************************
+ * Project: SawMill
+ * Authors:
+ *          Bart Meuris <bart . meuris @ gmail . com>
+ *
+ * License: (to be discussed LGPL version 2.0?)
+ *
+ ***************************************************************************/
 #include <string>
 #include <iostream>
+#include <signal.h>
 #include <boost/program_options.hpp>
 
 #include "command.pb.h"
 #include "logevent.pb.h"
 
+#include "config.h"
+#include "sawmill.h"
+
 namespace po = boost::program_options;
+
+using namespace sawmill;
+
+
+SawMill::SawMill()
+	: initialized(false), configset()
+{
+}
+
+void SawMill::run(void)
+{
+	this->config().load();
+}
+
+void SawMill::showVersion(std::ostream &out)
+{
+	out << APP_NAME << " version " << VER_MAJOR << "." << VER_MINOR;
+#ifdef VER_BUILD
+	out << "." << VER_BUILD;
+#endif
+	out << " (built on ";
+	out << OS_NAME;
+#ifdef BUILD_MACHINE_NAME
+	out << ":" <<  BUILD_MACHINE_NAME;
+#endif
+#ifdef BUILD_DATETIME
+	out << ":"<< BUILD_DATETIME;
+#endif
+	out << ")";
+	out << std::endl;
+}
+
 
 int main(int argc, char* argv[])
 {
+	SawMill mill;
+
 	// Parse commandline arguments
 	po::options_description desc("Generic options");
 	desc.add_options()
 		("help,h", "Show this help message")
-		("config,c", po::value<std::string>(), "Specify a config file to use")
+		("version,v", "Show the version")
+		("config,c", po::value< std::vector<std::string> >(), "Specify a config file to use")
 	;
 	po::variables_map vm;
+
 	try {
 		po::store(po::parse_command_line(argc, argv, desc), vm);
 		po::notify(vm);
@@ -24,11 +72,25 @@ int main(int argc, char* argv[])
 		std::cout << desc << std::endl;
 		return 1;
 	}
-	if ( (argc <= 1) || (vm.count("help")) ) {
+	if ( vm.count("version")) {
+		mill.showVersion(std::cout);
+	}
+	if ( vm.count("help")) {
 		std::cout << desc << std::endl;
 		return 1;
 	}
 
-	std::cout << "Test" << std::endl;
+	// (Double) fork if necessary
+	
+	// Use boost::asio:::signal_set to handle signals/ctrl-c/...
+	
+	
+	// Add configuration options - multiple allowed
+	if (vm.count("config") > 0) {
+		mill.config().addConfigSource( vm["config"].as< std::vector< std::string> >() );
+	}
+	
+	mill.run();
+
 	return 0;
 }
---
+++ src/sawmill.h
@@ -0,0 +1,26 @@
+#ifndef __SAWMILL_H
+# define __SAWMILL_H
+
+#include <string>
+#include <vector>
+#include "configmanager.h"
+
+namespace sawmill {
+
+class SawMill
+{
+public:
+	SawMill();
+
+	void showVersion(std::ostream &out);
+	ConfigManager &config() { return configset; }
+
+	void run();
+private:
+	bool initialized;
+	ConfigManager configset;
+};
+
+} // namespace sawmill
+
+#endif // ifndef __SAWMILL_H
