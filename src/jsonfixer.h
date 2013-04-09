5a11a2bfcf416f73acafe38afc778c5788384b28
bartmeuris <bart.meuris@gmail.com>
2013-04-10 00:26:26 +0200

    Fixed json tokenizer - testcase passes

--- src/jsonfixer.h
+++ src/jsonfixer.h
@@ -45,30 +45,12 @@ private:
 			case EOF:
 				go = false;
 				break;
-			case ' ':
-			case '\t':
-				// White space
-				break;
-			case '\n':
-			case '\r':
-				if (outlen == 0) {
-					out.put(std::cin.get());
-					outlen++;
-				}
-				go = false;
-				break;
-			case '"':
-				if (outlen != 0) {
-					go = false;
-					break;
-				}
-				// Fetch until a new " is encountered
-				while ( ( c = std::cin.get()) != EOF ) {
-					if (c == '"') {
-						
-					}
-				}
-				break;
+		// Generic single character separators
+			case '[':
+			case ']':
+			case '{':
+			case '}':
+			case ':':
 			case ',':
 				if (outlen == 0) {
 					out.put(std::cin.get());
@@ -76,41 +58,130 @@ private:
 				}
 				go = false;
 				break;
-			case '/':
-				c = std::cin.get();
-				if (std::cin.peek() == '/') {
-					if (outlen != 0) {
-						std::cin.unget();
-						go = false;
-						break;
+		// White space
+			case ' ':
+			case '\t':
+				// White space
+				// 1. if input empty -> eat all whitespace, then break
+				// 2. if input not empty -> break
+				if (outlen != 0) {
+					go = false;
+					break;
+				}
+				do {
+					prev = std::cin.get();
+					if (!stripwhitespace) {
+						out.put(prev);
+						outlen++;
 					}
+					c = std::cin.peek();
+				} while ((c == ' ') || (c == '\t'));
+				if (!stripwhitespace) {
+					go = false;
+				}
+				break;
+		// New lines
+			case '\n':
+			case '\r':
+				if (outlen == 0) {
+					// Send "newline" token
+					do {
+						std::cin.get();
+						c = std::cin.peek();
+					} while ((c == '\n') || (c == '\r'));
+					out.put('\n');
+					outlen++;
+				}
+				// Always end of token.
+				go = false;
+				break;
+		// Strings
+			case '"':
+				if (outlen != 0) {
+					go = false;
+					break;
+				}
+				// Fetch until a new " is encountered
+				do {
+					prev = std::cin.get();
+					out.put(prev);
+					outlen++;
+					c = std::cin.peek();
+				} while ( ( c != EOF ) && ( ( c != '"' ) || ( ( c == '"' ) && (prev == '\\') ) ) );
+				std::cin.get();
+				out.put('"');
+				outlen++;
+				go = false;
+				break;
+		// Comments
+			case '/':
+				if (outlen != 0) {
+					go = false;
+					break;
+				}
+				prev = std::cin.get();
+				c = std::cin.peek();
+				if (c == '/') {
+					// Line comment
 					if (!stripcomments) {
-						out.put(c);
+						out.put(prev);
+						outlen++;
 					}
 					while (!std::cin.eof()) {
 						c = std::cin.get();
 						if ((c == '\n') || (c == '\r')) {
-							std::cin.unget();
 							if (!stripcomments) {
+								// Don't eat newlines if comments are not stripped
+								std::cin.unget();
 								go = false;
 							}
 							break;
 						} else if (!stripcomments) {
 							out.put(c);
+							outlen++;
 						}
 					}
 				} else if (c == '*') { 
-
+					// Block comment
+					if (!stripcomments) {
+						out.put(prev);
+						outlen++;
+						out.put(c);
+						outlen++;
+					}
+					prev = std::cin.get(); // Flush '*'
+					c = std::cin.get();
+					if (c == EOF) {
+						go = false;
+						break;
+					}
+					do {
+						if (!stripcomments) {
+							out.put(c);
+							outlen++;
+						}
+						prev = c;
+						c = std::cin.get();
+					} while ( ( c != EOF ) && ( prev != '*' ) && ( c != '/' ) );
+					if (c == '/') {
+						if (!stripcomments) {
+							out.put(c);
+							outlen++;
+						}
+					}
+					if (!stripcomments) {
+						go = false;
+					}
 				} else if (c == EOF) {
-					out.put(c);
-					outlen++;
-					break;
+					go = false;
 				} else {
 					out.put(prev);
 					outlen++;
 				}
 				break;
+		// Default output ?
 			default:
+				out.put(std::cin.get());
 				break;
 			}
 		}
@@ -128,6 +199,18 @@ private:
 
 	void do_filter()
 	{
+		std::string ret;
+		do {
+			ret = getToken();
+			/*
+			if (ret == "\n") {
+				std::cout << "Token: --\\n--\n";
+			} else {
+				std::cout << "Token: --" << ret << "--\n";
+			}
+			*/
+			std::cout << ret;
+		} while (ret != "");
 	}
 
 	void do_filter_comments()
